da293e86ff0f53ea72e3fdc89fad56c312b76f5a
Jon <jonducrou@gmail.com>
2014-12-07 08:22:53 +0000

    keep going even on failures

--- Pi/TempOverI2C/temp_over_i2c.py
+++ Pi/TempOverI2C/temp_over_i2c.py
@@ -14,9 +14,13 @@ def read():
   return (t>>4) * 0.0625
 
 while True:
+ try:
   t = read()
   print t
   f = urllib.urlopen("http://www.devicehub.net/io/537/?apiKey=" + key + "&WineRoom=" + str(t))
   s = f.read()
   print s
-  time.sleep(60*60)
+ except:
+  print "looping in spite of failure"
+  pass 
+ time.sleep(60*60)
