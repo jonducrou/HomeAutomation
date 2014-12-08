8d0a363ffadd362ff14f5988f0ab0fb679cb29fe
Jon <jonducrou@gmail.com>
2014-12-08 08:31:07 +0000

    remove loop and rely on cron

--- Pi/TempOverI2C/temp_over_i2c.py
+++ Pi/TempOverI2C/temp_over_i2c.py
@@ -13,14 +13,9 @@ def read():
   t = (msb<<8) | lsb
   return (t>>4) * 0.0625
 
-while True:
- try:
-  t = read()
-  print t
-  f = urllib.urlopen("http://www.devicehub.net/io/537/?apiKey=" + key + "&WineRoom=" + str(t))
-  s = f.read()
-  print s
- except:
-  print "looping in spite of failure"
-  pass 
- time.sleep(60*60)
+
+t = read()
+print t
+f = urllib.urlopen("http://www.devicehub.net/io/537/?apiKey=" + key + "&WineRoom=" + str(t))
+s = f.read()
+print s
